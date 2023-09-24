d56b2406f1699fb5c84c4171fdde33b80aff3865
minpyaythein <paukking@gmail.com>
2023-09-24 19:08:22 +0900

    adding ui, attack, special attack, heal skills

---
+++ app.js
@@ -0,0 +1,62 @@
+const app = Vue.createApp({
+	data() {
+		return {
+			playerHealth: 100,
+			monsterHealth: 100,
+			currentRound: 0,
+			cooldown: {
+				specialAttack: 0,
+				heal: 0,
+			},
+		}
+	},
+	computed: {
+		playerHealthBar() {
+			return { width: this.playerHealth + '%' }
+		},
+		monsterHealthBar() {
+			return { width: this.monsterHealth + '%' }
+		},
+	},
+	methods: {
+		getRandomValue(max, min) {
+			return Math.floor(Math.random() * (max - min)) + min
+		},
+		attackMonster() {
+			this.currentRound++
+			const healthValue = this.getRandomValue(5, 12)
+			this.monsterHealth -= healthValue
+			this.attackPlayer()
+			// if (this.cooldown > 0) {
+			// 	this.cooldown--
+			// }
+			// this.cooldown = this.cooldown > 0 ? --this.cooldown : this.cooldown
+			Object.keys(this.cooldown).forEach((type) => {
+				this.cooldown[type] = Math.max(0, --this.cooldown[type])
+			})
+		},
+		attackPlayer() {
+			const healthValue = this.getRandomValue(8, 13)
+			this.playerHealth -= healthValue
+		},
+		specialAttackMonster() {
+			this.currentRound++
+			const healthValue = this.getRandomValue(10, 25)
+			this.monsterHealth -= healthValue
+			this.attackPlayer()
+			this.cooldown.specialAttack += 3
+			this.cooldown.heal = Math.max(0, --this.cooldown.heal)
+		},
+		healPlayer() {
+			this.currentRound++
+			const healthValue = this.getRandomValue(10, 20)
+			// this.playerHealth = Math.min(this.playerHealth + this.healthValue, 100);
+			this.playerHealth = this.playerHealth + healthValue > 100 ? 100 : this.playerHealth + healthValue
+			this.attackPlayer()
+			this.cooldown.heal += 3
+			this.cooldown.specialAttack = Math.max(0, --this.cooldown.specialAttack)
+		},
+	},
+})
+
+app.mount('#game')
